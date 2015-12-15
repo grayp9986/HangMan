b83da88ba8c99ca708f2c911ae4c048af66b2f25
grayp9986 <grayp9986@FHC-IC37281.wrdsb.ca>
2015-12-15 09:41:22 -0500

    Newest update to the code. Played around with button names and labels.

--- src/FraMain.java
+++ src/FraMain.java
@@ -4,7 +4,7 @@ public class FraMain extends JFrame{
     public FraMain(){
         PanMain panMain = new PanMain();
         add(new PanMain());
-        setTitle("Board");
+        setTitle("Hangman by Porter Gray");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(1200,400);
         this.setResizable(true);

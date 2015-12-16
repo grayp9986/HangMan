643ea83cea1b7cb0b5b38b65a4df55c6f82c3f9a
grayp9986 <grayp9986@FHC-IC0026816.wrdsb.ca>
2015-12-16 09:31:21 -0500

    Added panels for setting, and "board"

---
+++ src/PanDisp.java
@@ -0,0 +1,23 @@
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author grayp9986
+ */
+public class PanDisp extends JPanel{
+
+    PanDisp(JLabel lblOutput, JButton btnUpdateLabel) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    public void PanDisp(JLabel _lblOutput,JButton _btnUpdateLabel){
+        
+    }
+}
